1d9ac453b5facf8f3204c1daa0de67fe4ba6866d
Gousset <victor.gousset@epitech.eu>
2020-09-09 14:39:14 +0200

    Toggle button Like/Dislike

--- particeep/src/Component/Home/Home.js
+++ particeep/src/Component/Home/Home.js
@@ -1,6 +1,8 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 
 import './Home.css';
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
 
 class Home extends Component {
 
@@ -8,42 +10,92 @@ class Home extends Component {
         super(props);
 
         this.state = {
-            movies: []
+            movies: [],
+            category: [],
+            filter: [],
+            DefaultLike: false,
+            LikeDislike: []
         }
-    }
 
+    }
 
     componentDidMount() {
         import('./movies').then((movies$) => {
             Promise.resolve(movies$).then((value) => {
                 Promise.resolve(value.movies$).then((response) => {
                     this.setState({movies: response})
+                    for (let i = 0; i < this.state.movies.length; i++)
+                    {
+                        if (!this.state.category.find(element => element === this.state.movies[i].category)) {
+                            this.setState({category: [...this.state.category, this.state.movies[i].category]})
+                        }
+
+                        this.setState({LikeDislike: [...this.state.LikeDislike, false]})
+                        console.log(this.state.LikeDislike)
+                    }
                 })
             })
-
         }).catch(error => console.log(error))
     }
 
-    test = (id) => {
+    DeleteCard = (id) => {
         let id_supp = id - 1;
         delete this.state.movies[id_supp]
-        
+
         let new_tab = this.state.movies
         this.setState({movies: {}})
         this.setState({movies: new_tab})
     }
 
+    FilterChange = (name) => {
+        if (!this.state.category.find(element => element === name)) {
+            this.state.filter.push(name)
+            let id_supp = this.state.filter.indexOf(name)
+            delete this.state.filter[id_supp]
+        } else {
+            this.state.filter.push(name)
+        }
+
+        console.log(this.state.filter)
+    }
+
+    HandleChangeLikeDislike = (id) => {
+        let id_like = parseInt(id) - 1;
+        console.log(id_like)
+        if(this.state.LikeDislike[id_like] === false) {
+            this.state.LikeDislike[id_like] = true; //like
+            let last_number_like = parseInt(this.state.movies[id_like].likes)
+            let last_array = this.state.movies;
+            this.setState({movies: {}})
+            this.setState({movies: last_array})
+            this.state.movies[id_like].likes = last_number_like + 1;
+        } else {
+            this.state.LikeDislike[id_like] = false; //dislike
+            let last_number_like = parseInt(this.state.movies[id_like].likes)
+            let last_array = this.state.movies;
+            this.setState({movies: {}})
+            this.setState({movies: last_array})
+            this.state.movies[id_like].likes = last_number_like - 1;
+        }
+
+    }
+
     render() {
         return (
             <div className="Home">
+                {
+                    this.state.category.map((name) => {
+                        return ( <label><input type="checkbox" onChange={() => { this.FilterChange({name})}} name={name}></input>{name}</label> );
+                    })
+                }
                 <div className="row">
                 {
-                    this.state.movies.map(function (i) {
+                    this.state.movies.map(function(i) {
 
                         return ( <div className="column">
                                 <div key={i} className="Home_card">
                                     <h3 className="Home_card_title">{i.title}</h3>
-                                    <div className="Home_card_title_bar">
+                                    <div className="Home_card_title_bar" id={'test'}>
                                         <div className="Home_card_title_bar_like"
                                              style={{height: 25, width: (i.likes / (i.likes + i.dislikes) * 100 + '%')}}>
                                         </div>
@@ -51,7 +103,12 @@ class Home extends Component {
                                              style={{height: 25, width: (i.dislikes / (i.dislikes + i.likes) * 100 + '%')}}>
                                         </div>
                                     </div>
-                                    <div><button onClick={() => { this.test(i.id) }} >X</button></div>
+                                    <Toggle
+                                        id='cheese-status'
+                                        defaultChecked={this.state.DefaultLike}
+                                        onChange={() => {this.HandleChangeLikeDislike(i.id)}} />
+
+                                    <div><button onClick={() => { this.DeleteCard(i.id) }} >X</button></div>
                                 </div>
                                 </div> );
                     }, this)
